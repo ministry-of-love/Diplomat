19e5f12eca0a60a2e7e585f695ccfa50baa395eb
Yates Monteith <jymonte@clemson.edu>
2016-06-01 00:10:16 -0400

    Pre-running update

--- test_diplomat.py
+++ test_diplomat.py
@@ -1,18 +1,33 @@
-import os
-import unittest
 import diplomat
 
 from errbot.backends.test import testbot
+from errbot import plugin_manager
 
 class TestDiplomat(object):
     extra_plugin_dir = '.'
 
     def test_isred(self, testbot):
-        testbot.push_message('!isred Boneytooth Thompkins Isk-Chip')
-        expected = 'Boneytooth Thompkins Isk-Chip does not exist.'
+        # testbot.push_message('!diplomat isred')
+        # expected = "Please include a name of a person to check."
+        # result = testbot.pop_message()
+        # assert expected == result
+        
+        testbot.push_message('!diplomat isred --name Logical Fallacy')
+        expected = 'Logical Fallacy does not exist.'
         result = testbot.pop_message()
         assert expected == result
 
+        testbot.push('!isred --name Brad Neece')
+        expected = "Brad Neece\nStanding: -10.0" + \
+            "\nTags: annoying, edgey, misunderstood, summercamp"
+        result = testbot.pop_message()
+        assert expected == result
+
+        testbot.push('!isred --name warr akini')
+        expected = "Warr Akini\nStanding: -10.0" +\
+            "\nTags: cool, dick, butt, dickbutt"
+        result = testbot.pop_message()
+        assert expected == result
 
     def test_addtag(self, testbot):
         testbot.push_message('!diplomat addtag ____ ____')
