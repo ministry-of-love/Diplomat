50cf4cc65895c1d87f80ea8d040faa89a2098895
Yates Monteith <jymonte@clemson.edu>
2016-06-04 01:06:10 -0400

    Added additional V&V work to unit tests

--- test_diplomat.py
+++ test_diplomat.py
@@ -17,6 +17,13 @@ class TestDiplomat(object):
                    "\nTags: cool, multiboxer, spy"
         result = testbot.pop_message()
         assert expected == result
+        diplomat_plugin = testbot.bot.plugin_manager.get_plugin_obj_by_name('Diplomat')
+        assert 'logical fallacy' in diplomat_plugin.diplomat
+        assert 'spy' in diplomat_plugin.diplomat['logical fallacy']['tags']
+        assert 'multiboxer' in diplomat_plugin.diplomat['logical fallacy']['tags']
+        assert 'cool' in diplomat_plugin.diplomat['logical fallacy']['tags']
+        assert diplomat_plugin.diplomat['logical fallacy']['standing'] == 10.0
+
 
         testbot.push_message('!diplomat isred Fabulous Andy')
         expected = "fabulous andy does not exist in Diplomat."
@@ -29,11 +36,26 @@ class TestDiplomat(object):
         result = testbot.pop_message()
         assert expected == result
 
+        assert 'brad neece' in diplomat_plugin.diplomat
+        assert 'annoying' in diplomat_plugin.diplomat['brad neece']['tags']
+        assert 'edgey' in diplomat_plugin.diplomat['brad neece']['tags']
+        assert 'misunderstood' in diplomat_plugin.diplomat['brad neece']['tags']
+        assert 'summercamp' in diplomat_plugin.diplomat['brad neece']['tags']
+        assert 'bff' in diplomat_plugin.diplomat['brad neece']['tags']
+        assert diplomat_plugin.diplomat['brad neece']['standing'] == -5.0
+
         testbot.push_message('!diplomat isred warr akini')
         expected = "warr akini\nStanding: 10.0" +\
             "\nTags: cool, dick, butt, dickbutt"
         result = testbot.pop_message()
         assert expected == result
+
+        assert 'warr akini' in diplomat_plugin.diplomat
+        assert 'cool' in diplomat_plugin.diplomat['warr akini']['tags']
+        assert 'dick' in diplomat_plugin.diplomat['warr akini']['tags']
+        assert 'butt' in diplomat_plugin.diplomat['warr akini']['tags']
+        assert 'dickbutt' in diplomat_plugin.diplomat['warr akini']['tags']
+        assert diplomat_plugin.diplomat['warr akini']['standing'] == 10.0
 
     def test_addtag(self, testbot):
         # Test for empty argument string
@@ -106,9 +128,10 @@ class TestDiplomat(object):
         testbot.push_message('!diplomat remtag cool boneytooth thompkins isk-chip')
         expected = "Removed tag 'cool' from character 'boneytooth thompkins isk-chip'"
         result = testbot.pop_message()
-        # diplomat_plugin = plugin_manager.plugin_manager.get_plugin_obj_by_name('Diplomat')
-        # assert 'spy' not in diplomat_plugin.diplomat['boneytooth thompkins isk-chip']['tags']
-        # assert expected == result
+        assert expected == result
+        plugin = testbot.bot.plugin_manager.get_plugin_obj_by_name('Diplomat')
+        assert 'spy' not in plugin.diplomat['boneytooth thompkins isk-chip']['tags']
+
 
     def test_remchar(self, testbot):
         # Test for empty argument string
@@ -133,8 +156,8 @@ class TestDiplomat(object):
         expected = "Removed character 'boneytooth thompkins isk-chip' from Diplomat."
         result = testbot.pop_message()
         assert expected == result
-        # diplomat_plugin = plugin_manager.get_plugin_obj_by_name('Diplomat')
-        # assert 'boneytooth thompkins isk-chip' not in diplomat_plugin.diplomat
+        plugin = testbot.bot.plugin_manager.get_plugin_obj_by_name('Diplomat')
+        assert 'boneytooth thompkins isk-chip' not in plugin.diplomat
 
     def test_addchar(self, testbot):
         # Test for empty argument string
@@ -179,6 +202,10 @@ class TestDiplomat(object):
         expected = "Added Character: 'sjugar02'\nStanding: 10.0\nTags: cool"
         result = testbot.pop_message()
         assert expected == result
+        plugin = testbot.bot.plugin_manager.get_plugin_obj_by_name('Diplomat')
+        assert 'sjugar02' in plugin.diplomat
+        assert '10.0' == plugin.diplomat['sjugar02']['standing']
+        assert 'cool' in plugin.diplomat['sjugar02']['tags']
 
         # Test for adding character which already exists within Diplomat.
         # Expected Result: Error message is returned.
@@ -189,12 +216,12 @@ class TestDiplomat(object):
         expected = "Character 'boneytooth thompkins isk-chip' already exists in Diplomat."
         result = testbot.pop_message()
         assert expected == result
-        # diplomat_plugin = plugin_manager.get_plugin_obj_by_name('Diplomat')
-        # assert 'douchey' not in diplomat_plugin.diplomat['boneytooth thompkins isk-chip']['tags']
-        # assert 'cool' in diplomat_plugin.diplomat['boneytooth thompkins isk-chip']['tags']
-        # assert 'sexy' in diplomat_plugin.diplomat['boneytooth thompkins isk-chip']['tags']
-        # assert 'good' in diplomat_plugin.diplomat['boneytooth thompkins isk-chip']['tags']
-        # assert diplomat_plugin.diplomat['boneytooth thompkins isk-chip']['standing'] == -10.0
+        diplomat_plugin = testbot.bot.plugin_manager.get_plugin_obj_by_name('Diplomat')
+        assert 'douchey' not in diplomat_plugin.diplomat['boneytooth thompkins isk-chip']['tags']
+        assert 'cool' in diplomat_plugin.diplomat['boneytooth thompkins isk-chip']['tags']
+        assert 'sexy' in diplomat_plugin.diplomat['boneytooth thompkins isk-chip']['tags']
+        assert 'good' in diplomat_plugin.diplomat['boneytooth thompkins isk-chip']['tags']
+        assert diplomat_plugin.diplomat['boneytooth thompkins isk-chip']['standing'] == -10.0
 
     def test_write_dictionary(self, testbot):
         pass
